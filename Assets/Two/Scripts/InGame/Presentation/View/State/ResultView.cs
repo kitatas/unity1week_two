2498aaadaec2e486496d0a29f3e8a1c3aa694fc7
kitatas <315kitatas10703@gmail.com>
2021-04-29 18:54:43 +0900

    add: 結果で勝者を表示

--- Assets/Two/Scripts/InGame/Presentation/View/State/ResultView.cs
+++ Assets/Two/Scripts/InGame/Presentation/View/State/ResultView.cs
@@ -1,15 +1,31 @@
+using System;
 using System.Threading;
 using Cysharp.Threading.Tasks;
+using TMPro;
 using Two.InGame.Application;
+using Two.InGame.Domain.UseCase.Interface;
+using UnityEngine;
+using VContainer;
 
 namespace Two.InGame.Presentation.View.State
 {
     public sealed class ResultView : BaseState
     {
+        [SerializeField] private TextMeshProUGUI resultText = default;
+
+        private IMatchingUseCase _matchingUseCase;
+
+        [Inject]
+        private void Construct(IMatchingUseCase matchingUseCase)
+        {
+            _matchingUseCase = matchingUseCase;
+        }
+
         public override GameState GetState() => GameState.Result;
 
         public override UniTask InitAsync(CancellationToken token)
         {
+            resultText.text = "";
             return base.InitAsync(token);
         }
 
@@ -20,6 +36,10 @@ namespace Two.InGame.Presentation.View.State
 
         public override async UniTask<GameState> TickAsync(CancellationToken token)
         {
+            // 死亡演出終了待ち
+            await UniTask.Delay(TimeSpan.FromSeconds(1.0f), cancellationToken: token);
+
+            resultText.text = $"winner: {_matchingUseCase.GetWinnerName()}";
             return GameState.None;
         }
 
